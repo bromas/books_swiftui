d93a7c33686842afa192a59dadfc20e5652383ff
Brian Thomas <bpthomas@pm.me>
2022-04-04 16:21:33 -0400

    Initial Commit

---
+++ books_swiftui/books_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  books_swiftuiApp.swift
+//  books_swiftui
+//
+//  Created by Brian Thomas on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct books_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
